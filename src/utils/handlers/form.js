85abe22018da605f99401471bde47c73b6963a4d
ngudbhav <ngudbhav05@hotmail.com>
2022-10-06 18:58:38 +0530

    fix: add vercel function

--- src/utils/handlers/form.js
+++ src/utils/handlers/form.js
@@ -1,9 +1,14 @@
 const SUCCESS_CODE = 200;
 
 export const submit = async (formData) => {
+  const object = {};
+  formData.forEach((value, key) => object[key] = value);
   const options = {
     method: 'POST',
-    body: formData,
+    body: JSON.stringify(object),
+    headers: {
+      'Content-Type': 'application/json',
+    },
   }
   const response = await window.fetch(
     '/api/contact', options,
