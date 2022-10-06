22d737f27a078c3449a689c55ada8f53c2f0ca9f
ngudbhav <ngudbhav05@hotmail.com>
2022-10-06 17:00:31 +0530

    fix: add vercel function

--- src/api/contact.js
+++ src/api/contact.js
@@ -4,14 +4,14 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 export default function contactFormHandler(request, response) {
   console.log(request.body);
   console.log(request.query);
-  const { name, email, message, website } = request.body;
+  const { body } = request;
   const msg = {
     to: process.env.TO_EMAIL,
     from: process.env.FROM_EMAIL,
     subject: 'New Form Submission',
     html: `
-      <strong>Name: ${name} Email: ${email} Message: ${message}</strong>
-      Sent from ${website}
+      <strong>Name: ${body.name} Email: ${body.email} Message: ${body.message}</strong>
+      Sent from ${body.website}
     `,
   }
   sgMail.send(msg);
