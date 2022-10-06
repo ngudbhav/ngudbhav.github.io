8b097270910b5b2b5bd2a735004362c674de29f5
ngudbhav <ngudbhav05@hotmail.com>
2022-10-06 19:58:57 +0530

    fix: final changes

--- src/api/contact.js
+++ src/api/contact.js
@@ -13,6 +13,7 @@ export default async function handler(req, res) {
       Sent from ${body.website}
     `,
   }
-  sgMail.send(msg).then(response => console.log(response)).catch((error) => console.log(error));
+  const response = await sgMail.send(msg);
+  console.log(response);
   return res.status(200).json({});
 }
