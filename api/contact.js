15080e6986685d5d6a3082ccdad539a61b5d9db6
ngudbhav <ngudbhav05@hotmail.com>
2022-10-06 18:21:33 +0530

    fix: add vercel function

--- api/contact.js
+++ api/contact.js
@@ -2,6 +2,7 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default async function handler(req, res) {
+  console.log(req);
   const { body } = req;
   console.log(body);
   const msg = {
