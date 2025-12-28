045aafc184a78e3e2f1eda57e674292556f1ad80
ngudbhav <ngudbhav05@hotmail.com>
2025-12-28 20:52:06 +0530

    fix: mailer to mailgun

--- src/api/contact.js
+++ src/api/contact.js
@@ -1,20 +1,40 @@
-import Mailgun from 'mailgun.js';
-const mailgun = new Mailgun(FormData);
-const mg = mailgun.client({username: 'api', key: process.env.SENDGRID_API_KEY});
+import fetch from "node-fetch";
+
+const API_KEY = process.env.SENDGRID_API_KEY;
+const DOMAIN = "ngudbhav.com";
+console.log(API_KEY);
 
 export default async function handler(req, res) {
   const { body } = req;
   console.log(body);
-  const msg = {
-    from: process.env.FROM_EMAIL,
-    to: [process.env.TO_EMAIL],
-    subject: 'New Form Submission',
-    html: `
+  const form = new FormData();
+  form.append("from", process.env.FROM_EMAIL);
+  form.append("to", process.env.TO_EMAIL);
+  form.append("subject", "New Form Submission");
+  form.append(
+    "html",
+    `
       <strong>Name: ${body.name} Email: ${body.email} Message: ${body.message}</strong>
       Sent from ${body.website}
-    `,
-  };
-  const response = await mg.messages.create("ngudbhav.com", msg);
-  console.log(response);
-  return res.status(200).json({});
+    `
+  );
+  try {
+    const response = await fetch(
+      `https://api.mailgun.net/v3/${DOMAIN}/messages`,
+      {
+        method: "POST",
+        headers: {
+          Authorization:
+            "Basic " + Buffer.from(`api:${API_KEY}`).toString("base64"),
+        },
+        body: form,
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+    return res.status(200).json({});
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Failed to send email" });
+  }
 }
