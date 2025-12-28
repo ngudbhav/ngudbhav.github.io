36b08b681d449e77fcd5ef6281fd925466275598
ngudbhav <ngudbhav05@hotmail.com>
2025-12-28 16:36:38 +0530

    fix: mailer to mailgun

--- api/contact.js
+++ api/contact.js
@@ -1,19 +1,20 @@
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(FormData);
+const mg = mailgun.client({username: 'api', key: process.env.SENDGRID_API_KEY});
 
 export default async function handler(req, res) {
   const { body } = req;
   console.log(body);
   const msg = {
-    to: process.env.TO_EMAIL,
     from: process.env.FROM_EMAIL,
+    to: [process.env.TO_EMAIL],
     subject: 'New Form Submission',
     html: `
       <strong>Name: ${body.name} Email: ${body.email} Message: ${body.message}</strong>
       Sent from ${body.website}
     `,
-  }
-  const response = await sgMail.send(msg);
+  };
+  const response = await mg.messages.create("ngudbhav.com", msg);
   console.log(response);
   return res.status(200).json({});
 }
